30120304d10af42535e57a689f964a8106a81480
binhqd <binhqd@smartosc.com>
2015-03-27 17:25:55 +0700

    update here

---
+++ prgo/ch2/ch2note.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+	const limit = 52 // type compatible with any number 
+	const top unit16 = 1421 // type unit 16
+	start := -19 // int 
+	end := int64(123123) // int64
+	var i int // value 0 type int 
+	var debug = false // bool
+	checkResults := true // bool 
+	stepSize := 1.5 // float 
+
+}	

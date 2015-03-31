60c39e78d84d442d1d9a1bd878494c2fb11f793b
binhqd <binhqd@smartosc.com>
2015-03-31 14:50:45 +0700

    update here

--- prgo/ch2/example1.go
+++ prgo/ch2/example1.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"sort"
-	"http"
+	"net/http"
+	"log"
 )
 
 type statistics struct {
@@ -53,17 +53,45 @@ func main() {
 }
 
 func homePage(writer http.ResponseWriter, request *http.Request) {
-	err := request.ParseForm()
-	fmt.Fprint(writer, pageTop, form)
-	if err != nil {
-		fmt.Fprint(writer, anError, err)
-	} else {
-		if numbers, message, ok := processRequest(request); ok {
-			stats := getStats(numbers)
-			fmt.Fprint(writer, formatStats(stats))
-		} else if message != "" {
-			fmt.Fprint(writer, anError, message)
-		}
-	}
-	fmt.Fprint(writer, pageBottom)
+err := request.ParseForm() // Must be called before writing response
+fmt.Fprint(writer, pageTop, form)
+if err != nil {
+fmt.Fprintf(writer, anError, err)
+} else {
+if numbers, message, ok := processRequest(request); ok {
+stats := getStats(numbers)
+fmt.Fprint(writer, formatStats(stats))
+} else if message != "" {
+fmt.Fprintf(writer, anError, message)
+}
+}
+fmt.Fprint(writer, pageBottom)
+}
+
+func processRequest(request *http.Request) ([]float64, string, bool) {
+var numbers []float64
+if slice, found := request.Form["numbers"]; found && len(slice) > 0 {
+text := strings.Replace(slice[0], ",", " ", -1)
+for _, field := range strings.Fields(text) {
+if x, err := strconv.ParseFloat(field, 64); err != nil {
+return numbers, "'" + field + "' is invalid", false
+} else {
+numbers = append(numbers, x)
+}
+}
+}
+if len(numbers) == 0 {
+return numbers, "", false // no data first time form is shown
+}
+return numbers, "", true
+}
+
+func formatStats(stats statistics) string {
+return fmt.Sprintf(`<table border="1">
+<tr><th colspan="2">Results</th></tr>
+<tr><td>Numbers</td><td>%v</td></tr>
+<tr><td>Count</td><td>%d</td></tr>
+<tr><td>Mean</td><td>%f</td></tr>
+<tr><td>Median</td><td>%f</td></tr>
+</table>`, stats.numbers, len(stats.numbers), stats.mean, stats.median)
 }
